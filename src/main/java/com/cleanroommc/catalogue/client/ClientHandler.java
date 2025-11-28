214739a282b4bd92350cb94703474412e722f2e9
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-11-28 21:53:44 +0800

    Fix parent screen

--- src/main/java/com/cleanroommc/catalogue/client/ClientHandler.java
+++ src/main/java/com/cleanroommc/catalogue/client/ClientHandler.java
@@ -4,12 +4,15 @@ import com.cleanroommc.catalogue.CatalogueConfig;
 import com.cleanroommc.catalogue.CatalogueConstants;
 import com.cleanroommc.catalogue.client.screen.CatalogueModListScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.client.GuiModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
 
 /**
  * Author: MrCrayfish
@@ -18,8 +21,18 @@ import org.jetbrains.annotations.NotNull;
 public class ClientHandler {
     @SubscribeEvent
     public static void onOpenScreen(@NotNull GuiOpenEvent event) {
-        if (CatalogueConfig.enableMod && event.getGui() instanceof GuiModList) {
-            event.setGui(new CatalogueModListScreen(Minecraft.getMinecraft().currentScreen));
+        if (!CatalogueConfig.enableMod) return;
+        if (event.getGui() instanceof GuiModList screen) {
+            GuiScreen parent;
+            try {
+                Field field = GuiModList.class.getDeclaredField("mainMenu");
+                field.setAccessible(true);
+                parent = (GuiScreen) field.get(screen);
+            } catch (Exception e) {
+                CatalogueConstants.LOG.error("Failed to get field mainMenu from GuiModList", e);
+                parent = Minecraft.getMinecraft().currentScreen;
+            }
+            event.setGui(new CatalogueModListScreen(parent));
         }
     }
 }
