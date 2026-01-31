af0f8aa9f440a0b05d7dcf876545f6f4f43408a0
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-01-31 17:41:23 +0800

    Fix suggestion sometimes not updated

--- src/main/java/com/cleanroommc/catalogue/client/screen/CatalogueModListScreen.java
+++ src/main/java/com/cleanroommc/catalogue/client/screen/CatalogueModListScreen.java
@@ -164,7 +164,6 @@ public class CatalogueModListScreen extends GuiScreen implements DropdownMenuHan
         this.searchTextField.setResponder(s -> {
             if (!OPTION_QUERY.getValue().equals(s)) {
                 OPTION_QUERY.setValue(s);
-                this.updateSearchFieldSuggestion(s);
                 this.modList.filterAndUpdateList();
             }
         });
@@ -203,7 +202,6 @@ public class CatalogueModListScreen extends GuiScreen implements DropdownMenuHan
                 this.modList.centerScrollOn(entry);
             }
         }
-        this.updateSearchFieldSuggestion(this.searchTextField.getText());
     }
 
     @Override
@@ -526,6 +524,7 @@ public class CatalogueModListScreen extends GuiScreen implements DropdownMenuHan
                 Optional<ModListEntry> selectedEntry = this.children().stream().filter(entry -> entry.data == CatalogueModListScreen.this.selectedModData).findFirst();
                 selectedEntry.ifPresent(this::setSelected);
             }
+            CatalogueModListScreen.this.updateSearchFieldSuggestion(CatalogueModListScreen.this.searchTextField.getText());
             this.clampAmountScrolled();
         }
 
