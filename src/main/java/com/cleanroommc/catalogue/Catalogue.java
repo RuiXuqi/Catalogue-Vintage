a842a2cbddc2f885a93b1bf05e352f03cd354bd4
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-02-12 16:20:59 +0800

    Use customProperties

--- src/main/java/com/cleanroommc/catalogue/Catalogue.java
+++ src/main/java/com/cleanroommc/catalogue/Catalogue.java
@@ -5,6 +5,18 @@ import net.minecraftforge.fml.common.Mod;
 /**
  * Author: MrCrayfish
  */
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, clientSideOnly = true, acceptableRemoteVersions = "*")
+@Mod(
+        modid = Reference.MOD_ID,
+        name = Reference.MOD_NAME,
+        version = Reference.VERSION,
+        clientSideOnly = true,
+        acceptableRemoteVersions = "*",
+        customProperties = {
+                @Mod.CustomProperty(k = "license", v = "MIT"),
+                @Mod.CustomProperty(k = "issueTrackerUrl", v = "https://github.com/RuiXuqi/Catalogue-Vintage/issues"),
+                @Mod.CustomProperty(k = "iconFile", v = "assets/catalogue/icon.png"),
+                @Mod.CustomProperty(k = "backgroundFile", v = "assets/catalogue/background.png")
+        }
+)
 public class Catalogue {
 }
--- src/main/java/com/cleanroommc/catalogue/client/ForgeModData.java
+++ src/main/java/com/cleanroommc/catalogue/client/ForgeModData.java
@@ -79,22 +79,25 @@ public class ForgeModData implements IModData {
     @Nullable
     @Override
     public String getItemIcon() {
-        //return this.metadata != null ? this.metadata.iconItem : null;
-        return null;
+//        return this.metadata != null ? (this.metadata.iconItem.trim().isEmpty() ?
+//                this.info.getCustomModProperties().get("iconItem") : null) : null;
+        return this.info.getCustomModProperties().get("iconItem");
     }
 
     @Nullable
     @Override
     public String getImageIcon() {
-        //return this.metadata != null ? this.metadata.iconFile : null;
-        return null;
+//        return this.metadata != null ? (this.metadata.iconFile.trim().isEmpty() ?
+//                this.info.getCustomModProperties().get("iconFile") : null) : null;
+        return this.info.getCustomModProperties().get("iconFile");
     }
 
     @Nullable
     @Override
     public String getLicense() {
-        //return this.metadata != null ? this.metadata.license : null;
-        return null;
+//        return this.metadata != null ? (this.metadata.license.trim().isEmpty() ?
+//                this.info.getCustomModProperties().get("license") : null) : null;
+        return this.info.getCustomModProperties().get("license");
     }
 
     @Nullable
@@ -118,8 +121,9 @@ public class ForgeModData implements IModData {
     @Nullable
     @Override
     public String getIssueTracker() {
-        //return this.metadata != null ? this.metadata.issueTrackerUrl : null;
-        return null;
+//        return this.metadata != null ? (this.metadata.issueTrackerUrl.trim().isEmpty() ?
+//                this.info.getCustomModProperties().get("issueTrackerUrl") : null) : null;
+        return this.info.getCustomModProperties().get("issueTrackerUrl");
     }
 
     @Nullable
@@ -131,8 +135,9 @@ public class ForgeModData implements IModData {
     @Nullable
     @Override
     public String getBackground() {
-        //return this.metadata != null ? this.metadata.backgroundFile : null;
-        return null;
+//        return this.metadata != null ? (this.metadata.backgroundFile.trim().isEmpty() ?
+//                this.info.getCustomModProperties().get("backgroundFile") : null) : null;
+        return this.info.getCustomModProperties().get("backgroundFile");
     }
 
     @Nullable
